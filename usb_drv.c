381ab04be301807d9ddff3611d1765963d1dc7b7
Vitalii Pohribnyi <vb.pogribnyi@gmail.com>
2026-03-04 01:31:38 -0600

    Grabs sound data from USB device.

--- usb_drv.c
+++ usb_drv.c
@@ -5,9 +5,13 @@
 #include <net/sock.h>
 #include <linux/delay.h>
 #define NETLINK_USER 31
+#define START_SOUND_TRANSMIT 0x25
+#define END_SOUND_TRANSMIT 0x26
+
+#define BLK_SIZE (32*1024)
 
 const struct usb_device_id usb_drv_id_table[] = {
-    {USB_DEVICE(0x0483, 0x1234)},
+    {USB_DEVICE_INTERFACE_CLASS(0x0477, 0x1122, 0XFE)},
     {}
 };
 
@@ -16,9 +20,12 @@ static struct usb_device *usb_drv_device;
 static __u8 *usb_buffer;
 // static bool is_probing = false;
 static struct urb *urb;
+static struct urb *urb_blk;
 static __u8 *urb_buffer;
+static __u8 *urb_blk_buffer;
 static struct sock *nl_sock;
 static int pid;
+static int is_blk_active = 0;
 
 uint8_t* tmp_buff;
 
@@ -29,27 +36,27 @@ static void nl_receive(struct sk_buff *skb) {
     printk("Received message from pid %i: %s\n", pid, (char*)nlmsg_data(nlh));
 }
 
-int try_send_audio(uint8_t* buff, uint16_t count) {
+int receive_bulk(uint8_t* buff) {
     __u8 endpoint = 0;
     __u8 request = 0x03;    // SET_FEATURE
     __u8 requesttype = 0xC1;// TYPE_VENDOR | RECEPIENT_DEVICE
     __u16 index = 0;
-    __u16 value = count;
+    // __u16 value = count;
     int res = 0;
     unsigned int pipe;
     unsigned int pipe_blk;
-    int actual_length;
+    int actual_length = 0;
     pipe = usb_rcvctrlpipe(usb_drv_device, endpoint);
     pipe_blk = usb_sndbulkpipe(usb_drv_device, 2);
 
-    usb_control_msg(usb_drv_device, pipe, request, requesttype, value, index,
-        usb_buffer, 8, 5000);
+    // usb_control_msg(usb_drv_device, pipe, request, requesttype, value, index,
+    //    usb_buffer, 8, 5000);
     if (usb_buffer[0] == 1) {
         // Send audio using bulk transaction
-        printk("Sengind audio: %i bytes\n", count);
-        res = usb_bulk_msg(usb_drv_device, pipe_blk, buff, count, &actual_length, 1000);
-        if (res) printk("Error sending request: %i\n", res);
-        printk("Sent: %i\n", actual_length);
+        // printk("Sengind audio: %i bytes\n", count);
+        // res = usb_bulk_msg(usb_drv_device, pipe_blk, buff, count, &actual_length, 1000);
+        // if (res) printk("Error sending request: %i\n", res);
+        // printk("Sent: %i\n", actual_length);
         return actual_length;
     } else {
         // There's no space available on the device. Wait.
@@ -62,8 +69,8 @@ ssize_t usb_drv_read (struct file *file, char __user *buffer, size_t count, loff
     int actual_len = 0;
     
     __u8 endpoint = 0;
-    __u8 request = 0x00;    // GET_STATUS
-    __u8 requesttype = 0xC0;// TYPE_VENDOR | RECEPIENT_DEVICE
+    __u8 request = START_SOUND_TRANSMIT;
+    __u8 requesttype = 0xCC;// TYPE_VENDOR | RECEPIENT_DEVICE
     __u16 index = 0;
     __u16 value = 0;
     unsigned int pipe = usb_rcvctrlpipe(usb_drv_device, endpoint);
@@ -71,8 +78,10 @@ ssize_t usb_drv_read (struct file *file, char __user *buffer, size_t count, loff
     usb_control_msg(usb_drv_device, pipe, request, requesttype, value, index,
         usb_buffer, 8, 5000);
     // usb_interrupt_msg(usb_drv_device, pipe, usb_buffer, 8, &actual_len, 5000);
-    usb_buffer[0] += 48;
-    usb_buffer[1] = '\n';
+    // usb_buffer[0] += 48;
+    // usb_buffer[1] = '\n';
+
+    // receive_bulk(usb_buffer);
 
     res = copy_to_user(buffer, usb_buffer, 8);
     printk("Reading %lu bytes: %s (%i read)\n", count, usb_buffer, actual_len);
@@ -95,7 +104,7 @@ ssize_t usb_drv_write (struct file *file, const char __user *buffer, size_t coun
     // Do send in a loop
     while (bytes_sent < count && retries++ < 1000) {
         if (packet_size > count) packet_size = count;
-        bytes_sent += try_send_audio(tmp_buff + bytes_sent, packet_size);
+        //bytes_sent += try_send_audio(tmp_buff + bytes_sent, packet_size);
         printk("Total sent: %li bytes\n", bytes_sent);
         msleep(5);
     }
@@ -114,19 +123,48 @@ struct file_operations fops = {
     .write=usb_drv_write
 };
 
-static void usb_int_callback(struct urb* urb) {
-    struct sk_buff *skb_out = nlmsg_new(1, 0);
-    struct nlmsghdr *nlh;
-    int res;
-
-    nlh = nlmsg_put(skb_out, 0, 0, NLMSG_DONE, 1, 0);
-    NETLINK_CB(skb_out).dst_group = 0;
-    strncpy(nlmsg_data(nlh), urb_buffer, 1);
-    res = nlmsg_unicast(nl_sock, skb_out, pid);
-    if (res < 0) {
-        printk("NLMSG error: %i\n", res);
+static void usb_blk_callback(struct urb* urb) {
+    if (urb->status == 0) {
+        printk("Bulk received: %i bytes.\n", urb->actual_length);
+    } else {
+        printk("Transfer error: %i.\n", urb->status);
     }
-    printk("Interrupt happened: %i\n", urb_buffer[0]);
+    is_blk_active = 0;
+}
+
+static void usb_int_callback(struct urb* urb) {
+    // struct sk_buff *skb_out = nlmsg_new(1, 0);
+    // struct nlmsghdr *nlh;
+    // int res;
+    printk("Interrupt received.\n");
+
+
+    // Sound data ready
+    // unsigned int pipe_blk;
+    // int actual_length = 0;
+    // pipe_blk = usb_rcvbulkpipe(usb_drv_device, 1);
+
+    // res = usb_bulk_msg(usb_drv_device, pipe_blk, usb_buffer, BLK_SIZE, &actual_length, 1000);
+    // if (res) printk("Error sending request: %i\n", res);
+    if (is_blk_active) {
+        printk("Bulk URB remains active!\n");
+    } else {
+        if (usb_submit_urb(urb_blk, GFP_KERNEL)) {
+            printk("Failed to submit bulk URB\n");
+        } else {
+            is_blk_active = 1;
+        }
+    }
+    // printk("Received: %i\n", actual_length);
+
+    // nlh = nlmsg_put(skb_out, 0, 0, NLMSG_DONE, 1, 0);
+    // NETLINK_CB(skb_out).dst_group = 0;
+    // strncpy(nlmsg_data(nlh), urb_buffer, 1);
+    // res = nlmsg_unicast(nl_sock, skb_out, pid);
+    // if (res < 0) {
+    //    printk("NLMSG error: %i\n", res);
+    // }
+    // printk("Interrupt happened: %i\n", urb_buffer[0]);
     if (usb_submit_urb(urb, GFP_KERNEL)) {
         printk("Failed to RE-submit URB\n");
     }
@@ -137,6 +175,8 @@ int usb_drv_probe (struct usb_interface *intf, const struct usb_device_id *id) {
     int retval = 0;
     struct usb_host_endpoint *ep;
     unsigned int pipe;
+    struct usb_host_endpoint *ep_blk;
+    unsigned int pipe_blk;
 
     printk("Probing device\n");
     usb_drv_class.name = "usb/usbdrv_%d";
@@ -150,12 +190,18 @@ int usb_drv_probe (struct usb_interface *intf, const struct usb_device_id *id) {
 
     // Start interrupt requests
     urb = usb_alloc_urb(0, GFP_KERNEL);
-    pipe = usb_rcvintpipe(usb_drv_device, 1);
+    urb_blk = usb_alloc_urb(0, GFP_KERNEL);
+    pipe = usb_rcvintpipe(usb_drv_device, 2);
+    pipe_blk = usb_rcvbulkpipe(usb_drv_device, 1);
     urb_buffer = kmalloc(8, GFP_KERNEL);
+    urb_blk_buffer = kmalloc(BLK_SIZE, GFP_KERNEL);
 
 	ep = usb_pipe_endpoint(usb_drv_device, pipe);
+	ep_blk = usb_pipe_endpoint(usb_drv_device, pipe_blk);
     usb_fill_int_urb(urb, usb_drv_device, pipe, urb_buffer, 8, 
         usb_int_callback, NULL, ep->desc.bInterval);
+    usb_fill_bulk_urb(urb_blk, usb_drv_device, pipe_blk, urb_blk_buffer, BLK_SIZE, 
+        usb_blk_callback, NULL);
     if (usb_submit_urb(urb, GFP_KERNEL)) {
         kfree(urb_buffer);
         usb_free_urb(urb);
@@ -169,7 +215,9 @@ int usb_drv_probe (struct usb_interface *intf, const struct usb_device_id *id) {
 void usb_drv_disconnect(struct usb_interface *intf) {
     printk("Disconnecting device\n");
     kfree(urb_buffer);
+    kfree(urb_blk_buffer);
     usb_free_urb(urb);
+    usb_free_urb(urb_blk);
     usb_deregister_dev(intf, &usb_drv_class);
 }
 
@@ -185,19 +233,19 @@ int __init usb_drv_init(void) {
         .input=nl_receive,
     };
     printk("Initializing\n");
-    usb_buffer = kmalloc(8, GFP_KERNEL);
+    usb_buffer = kmalloc(16, GFP_KERNEL);
     usb_register(&usb_drv);
     
-    nl_sock = netlink_kernel_create(&init_net, NETLINK_USER, &cfg);
-    if (!nl_sock) {
-        printk("Unable to create socket\n");
-    }
+    // nl_sock = netlink_kernel_create(&init_net, NETLINK_USER, &cfg);
+    // if (!nl_sock) {
+    //     printk("Unable to create socket\n");
+    // }
     return 0;
 }
 
 void __exit usb_drv_exit(void) {
     printk("Exiting\n");
-    netlink_kernel_release(nl_sock);
+    // netlink_kernel_release(nl_sock);
     usb_deregister(&usb_drv);
     kfree(usb_buffer);
 }
